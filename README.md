683c0e514ed01f3d392380250c3d04f6d3d4d09e
ameroran <oran.amer@yahoo.com>
2019-11-28 23:30:39 +0200

    add CSS

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # lab11
+
+# We make a website for multiple images
+
+- write array of the images.
+
+- the constructor .
+
+- while loop to let the images don't show same in the same round.
+
+- algorithem for the images in the second round don't be the same in first round .
+
+- counter to know how much this image showed on the website.
+
+- chart fot the view and votes for these images.
+
--- app.js
+++ app.js
@@ -139,25 +139,8 @@ function list() {
                 data: vo,
                 
                 
-                backgroundColor: [
-                    'rgba(255, 99, 132, 1)',    
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                    
-                    
-                    
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',    
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
+                backgroundColor: "black",
+                borderColor: "black",
                 borderWidth: 1
                 
                 
@@ -167,27 +150,8 @@ function list() {
                 data: vi,
                 
                 
-                backgroundColor: [
-                    
-                    'rgba(255, 99, 132, 1)',    
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                    
-                    
-                    
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 0.2)',    
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    
-                ],
+                backgroundColor: "grey",
+                borderColor: "grey",
                 borderWidth: 1
                 
             }]
